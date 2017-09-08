2c5ddddb5d482cae86e3ea93e7f1799c2bccf17b
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-09-08 08:42:37 +0200

    Updated controller, added new views and fixed routes

---
+++ controller/fitnessController.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports.index = function (req, res) {
+    res.render('index', {title: "hello world"})
+};
+
+module.exports.createExercise = function (req, res) {
+
+};
+
+module.exports.createWorkout = function (req, res) {
+
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const controller = require('controller/fitnessController');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Fitness'});
-});
+router.get('/', controller.index);
+router.get('/workout', controller.createWorkout);
+router.get('/exercise', controller.createExercise);
 
 module.exports = router;
