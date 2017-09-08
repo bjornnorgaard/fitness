bada51d88a277a9e3811657a26732eaf2d2adab6
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-09-08 08:51:57 +0200

    Routing works!

--- controller/fitnessController.js
+++ controller/fitnessController.js
@@ -5,9 +5,9 @@ module.exports.index = function (req, res) {
 };
 
 module.exports.createExercise = function (req, res) {
-
+    res.render('exercise', {title: "hello world"})
 };
 
 module.exports.createWorkout = function (req, res) {
-
+    res.render('workout', {title: "hello world"})
 };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('controller/fitnessController');
+const controller = require('../controller/fitnessController');
 
 router.get('/', controller.index);
 router.get('/workout', controller.createWorkout);
